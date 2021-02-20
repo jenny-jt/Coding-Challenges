e51a80129c0e88dc98981255440f6d288eb3f886
Jenny Tan <tanjenny.yo@gmail.com>
2021-02-20 13:16:10 -0800

    added Trie problem, added Trie implementation with count of words that start with prefix

--- Problems/problems.md
+++ Problems/problems.md
@@ -35,3 +35,39 @@ These are very useful for autocomplete and the like
 5. Write a function that returns the lowest common ancestor of two nodes (the lowest node in T that has both n1 and n2 as descendants)
 
 6. Write a function that returns the lowest common ancestor of a list of nodes (the lowest node in T that has both n1 and n2 as descendants)
+
+7. Escape Room Keypads
+You are attempting to solve a puzzle in an Escape room with your team where you need to
+open a door to get to the next stage. There are several doors, each with a different keypad 
+on it. The keypads each have 7 keys, containing 7 distinct letters.
+
+Each kepad looks like this:
+
+The instructions state that one of the keypads will open the correct door leading to the next
+stage of the game. Your job is to find a word that unlocks the correct keypad. 
+
+After struggling for some time, the Escape room leader gave you a clue, that the first letter
+of the keypad is guaranteed to be in the word that opens the door. We will call this letter the
+"key" letter. The goal of this exercise is to come up with as many words as possible that your
+team can test out on the keypads and find the correct combination to go to the next stage of the
+game. How many owrds from the input wordlist canyou make out of the letters in each keypad?
+
+What you know:
+The correct combination will be a valid english word
+The words are at least 5 letters long, with no maximum length
+The key letter will be in the correct answer
+The words do not contain any letters outside the seven letters on the keypad
+The letters may be reused, including the "key" letters
+
+For our purposes, we will express each set of keypad letters as string of length 7, where the 
+first letter
+
+Constraints:
+both wordlist and keypad letters will be in all capital letters
+all words in wordlist are length 5 or greater
+every sequence of keypad letters will consist of 7 distinct letters
+
+Example 1
+input: ['APPLE', 'PLEAS', 'PLEASE'] 
+keypads: ['AELWXYZ', 'AELPXYZ', 'AELPSXY', 'SAELPRT', 'XAEBKSY'] 
+expected output: [0, 1, 3, 2, 0]
--- Solutions/Trees.py
+++ Solutions/Trees.py
@@ -184,3 +184,88 @@ def diagonal_sum():
         j += 1
 
 
+class TrieNode:
+    def __init__(self):
+        # staring from 0, diff with A 
+        self.children = [None] * 26
+        # each node has a boolean, changed to True after finish insert
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+        
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+		# insert each char in correct posititon and then move my curr
+        current = self.root
+
+        for char in word:
+            print("INSERT char", char)
+            if not current.children[ord(char) - ord('a')]:
+                # make new node if node does not exist
+                current.children[ord(char) - ord('a')] = TrieNode()
+            # move my current to that charcter
+            current = current.children[ord(char) - ord('a')]     
+        
+        current.isEnd = True # end insert True meaning the word is in tree
+        # print("current after INSERT", current.children, current.isEnd)
+
+	# Search for a char if not there then return False or if there move my curr
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        current = self.root
+        for char in word:
+            print("SEARCH char", char)
+            if not current.children[ord(char) - ord('a')]:
+                return False # that character is not present in that children
+            # moving along to child node
+            current = current.children[ord(char) - ord('a')]
+        # if the last node isEnd is true, then it has been inserted before    
+        return current.isEnd 
+        
+	# Search for a prefix if not there then return False or if there move my curr till my prefix ends and return True
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        current = self.root
+        for i in range(len(prefix)):
+            char = prefix[i]
+            # if the char is not a child, then it's not in the trie
+            if not current.children[ord(char) - ord('a')]:
+                return False 
+            current = current.children[ord(char) - ord('a')]
+        
+        num_children = [x for x in current.children if x]
+ 
+        return len(num_children)
+
+
+# Escape Room Keypads- supposed to use Trie
+
+def numKeypadSolutions(wordlist, keypads):
+    """input of 2 string arrays, output int array with num of words for each keypad"""
+    # initialize ans array that contains num of words for each keypad
+    ans = [0] * len(keypads)
+    
+    # convert each word in wordlist to a set
+    words = [set(word) for word in wordlist if len(set(word)) <= 7]
+    
+    # for each keypad, check if word is subset of keypad and if key in word
+    for i, keypad in enumerate(keypads):
+        key = keypad[0]
+        for word in words:
+            if key in word and word.issubset(set(keypad)):
+                ans[i] += 1
+
+    return ans
+
+print(numKeypadSolutions(['APPLE', 'PLEAS', 'PLEASE', 'DOG'], ['PBNEDAL', 'OESADLP', 'MOTLAJF', 'GBLKORD']))
