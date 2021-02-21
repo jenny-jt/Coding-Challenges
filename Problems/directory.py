d4a71fd12c891bdaf934775e1e368f2f4808d483
Jenny Tan <tanjenny.yo@gmail.com>
2021-02-21 14:54:38 -0800

    finished directory problem

--- Problems/directory.py
+++ Problems/directory.py
@@ -1,13 +1,32 @@
 # Given a set of strings that represent directories. Return a string representing
 # the part of the directory tree that is common to all of the directoriess
 
-def common_path(directories):
-    return []
-
 directories = [
-'/home/jenny/code/search/common/',
-'/home/jenny/code/api/client',
-'/home/jenny/code/service/app/tests'
-]
+    '/home/jenny/code/search/common/',
+    '/home/jenny/code/api/client',
+    '/home/jenny/code/service/app/tests'
+    ]
 
 ouput = '/home/jenny/code'
+
+
+def common_path(directories):
+    directory_list = []
+    # preprocess: split on "/"
+    dir_tree = [x.split("/") for x in directories]
+
+    # preprocess: get rid of empty strings
+    for item in dir_tree:
+        directory = [x for x in item if x]
+        directory_list.append(directory)
+
+    i = 0
+    while i < len(directory_list[0]):
+        for directory in directory_list:
+            if directory[i] != directory_list[0][i]:
+                return('/'.join(directory_list[0][:i]))
+        i += 1
+    return('/'.join(directory_list[0][:i]))
+
+
+print(common_path(directories))
--- Solutions/DP.py
+++ Solutions/DP.py
@@ -1,7 +1,5 @@
 # 3 1 8 2 5 -> 1 2 5
-
 # import doctests
-
 
 # L[3] = 2 
 # L[0] = 1
@@ -11,29 +9,16 @@
 # 5 2 8 6 3 6 9 5 -> 
 
 def find_inc(n):
-    """ takes array of int n, returns length of longest increasing subsequence
+    """ takes array of int n, returns length of longest increasing subsequence
     """
     dp = [1] * len(n)
     
-    for idx in range(len(n)):
-        # calculate subproblems 
-        # l[i] = 1 + max(subproblems)
+    for idx in range(len(n)):
+        # calculate subproblems 
+        # l[i] = 1 + max(subproblems)
         subproblems = [dp[k] for k in range(idx) if n[k] < n[idx]]
-        dp[idx] += max(subproblems, default=0)
+        dp[idx] += max(subproblems, default=0)
     
-    return max(dp, default=0)
+    return max(dp, default=0)
 
 print(find_inc([5 ,2 ,8, 6, 3, 6, 9, 5]))
-    
-def find_inc(n):
-    """ takes array of int n, returns longest increasing subsequence
-    """
-    dp = [1] * len(n)
-    
-    for idx in range(len(n)):
-        # calculate subproblems 
-        # l[i] = 1 + max(subproblems)
-        subproblems = [dp[k] for k in range(idx) if n[k] < n[idx]]
-        dp[idx] += max(subproblems, default=0)
-    
-    return max(dp, default=0)
